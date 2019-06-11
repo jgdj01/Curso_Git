ecb64de8ebf7df75efb962bf6520e0571d98da51
João Gabriel D. Joaquim <jgdjoaquim@hotmail.com>
2019-06-11 15:02:31 -0300

    Adicionando função de subtração

--- projeto.c
+++ projeto.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int subtracao(int a, int b){
+	int sub_num = a - b;
+	return sub_num;
+}
+
 int soma(int a, int b){
 	int soma_num = a + b;
 	return soma_num;
 }
 
+
 int main(){
-	printf("Some dos numeros: %d\n", soma(3,4));
-	
+	printf("Soma dos numeros: %d\n", soma(3,4));
+	printf("Subtracao dos numeros: %d\n", subtracao(5,5));
 	return 0;
 }
